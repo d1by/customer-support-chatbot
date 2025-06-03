800f8c59c055c8613020d8f6638e7e6cc0f596be
d1by <ninjaansh@gmail.com>
2025-06-03 19:59:16 +0530

    modified chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -14,7 +14,7 @@ import random
 nltk.download("wordnet")
 try:
     nlp = spacy.load("en_core_web_sm")
-except OSError:
+except:
     import subprocess
     subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
     nlp = spacy.load("en_core_web_sm")
